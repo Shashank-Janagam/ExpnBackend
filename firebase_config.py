b9eb7806b7c4b06664e2de86eb689ef4e83e646c
Shashank-Janagam <shashijanagam2004@gmail.com>
2025-09-21 19:19:49 +0530

    dd

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,11 @@
+import os
+import json
 import firebase_admin
-
 from firebase_admin import credentials, firestore
 
-cred=credentials.Certificate("serviceAccountKey.json")
+# Load Firebase credentials from environment variable
+firebase_config = json.loads(os.environ["FIREBASE_CONFIG"])
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred)
-db=firestore.client()
 
+db = firestore.client()
